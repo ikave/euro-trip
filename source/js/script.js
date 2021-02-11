3275e678bc76ece6432ec959d8ce84aba5ec01b5
kave <shiftgear@mail.ru>
2021-02-11 10:34:50 +0300

    Исправляет меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,20 @@
 'use strict';
-var noJs = document.querySelector('.no-js');
-noJs.classList.remove('no-js');
 
 var mainNav = document.querySelector('.main-nav');
 var mainNavToggle = document.querySelector('.main-nav__toggle');
 var menu = document.querySelector('.menu');
 
+mainNavToggle.classList.remove('main-nav__toggle--nojs');
+menu.classList.remove('menu--nojs');
 // mainNav.classList.remove('page-header--nojs');
 
 mainNavToggle.addEventListener('click', function () {
-  if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
+  if (mainNavToggle.classList.contains('main-nav__toggle--closed')) {
     mainNavToggle.classList.remove('main-nav__toggle--closed');
     mainNavToggle.classList.add('main-nav__toggle--opened');
     menu.classList.remove('menu--closed');
     menu.classList.add('menu--opened');
   } else {
-    mainNav.classList.add('main-nav--closed');
-    mainNav.classList.remove('main-nav--opened');
     mainNavToggle.classList.add('main-nav__toggle--closed');
     mainNavToggle.classList.remove('main-nav__toggle--opened');
     menu.classList.add('menu--closed');
